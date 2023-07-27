e03dec78a90d26f074bb919b8eebace206528e89
chinhnd7 <ndchinh110797@gmail.com>
2023-07-27 17:56:33 +0700

    update

---
+++ VotingContract.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.8.9;
+
+interface IToken {
+    function transferFrom(address, address, uint256) external returns (bool);
+    function approve(address, uint256) external returns (bool);
+    function balanceOf(address) external returns (uint256);
+    function totalSupply() external returns (uint256);
+}
+
+contract VotingContract {
+    struct Proposal {
+        string description;
+        uint256 yesCount;
+        uint256 noCount;
+        uint256 timestamp;
+    }
+
+    // IToken public votingToken;
+    IToken public votingToken;
+
+    Proposal[] public proposals;
+
+    uint256 public proposalCount;
+
+    mapping(address => mapping(uint256 => bool)) private hasVoted;
+    mapping(uint256 => bool) public resultProposal;
+
+    event CastVote(address, uint256);
+
+    modifier checkProposalEnded (uint256 proposalId){
+        require(block.timestamp < proposals[proposalId].timestamp, "Proposal is ended");
+        _;
+    }
+    constructor(address _votingToken) {
+        proposalCount = 0;
+        votingToken = IToken(_votingToken);
+    }
+
+    function createProposal(string memory _description) public {
+        votingToken.transferFrom(msg.sender, address(this), 20);
+        Proposal memory proposal = Proposal(
+            _description,
+            0,
+            0,
+            block.timestamp + 3 days
+        );
+        proposals.push(proposal);
+    }
+
+    // cast vote: bỏ phiếu
+    function castVote(uint256 proposalId, bool isApproved) public checkProposalEnded(proposalId) {
+        require(!hasVoted[msg.sender][proposalId], "Already voted");
+        uint256 totalToken = votingToken.balanceOf(msg.sender);
+        
+        if(isApproved) {
+            proposals[proposalId].yesCount += totalToken;
+        } else {
+            proposals[proposalId].noCount += totalToken;
+        }
+        hasVoted[msg.sender][proposalId] = true;
+        emit CastVote(msg.sender, proposalId);
+    }
+
+    function finalizeProposal(uint256 proposalId) public {
+        require(block.timestamp >= proposals[proposalId].timestamp, "Not ended");
+        uint256 yesCount = proposals[proposalId].yesCount;
+        uint256 totalSupply = votingToken.totalSupply();
+        uint256 totalYesVote = yesCount / totalSupply * 100;
+
+        if (totalYesVote > 50)
+    }
+}
